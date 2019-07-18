4070b2e692a1dc8b4c3cc0985aca503c39bf7561
Ryan Sundberg <ryan.sundberg@gmail.com>
2019-07-17 19:50:38 -0700

    Version 1.1.0-SNAPSHOT: upgraded curator to 4.2.1

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject arctype/service.curator "1.0.1"
+(defproject arctype/service.curator "1.1.0-SNAPSHOT"
   :dependencies 
   [[org.clojure/clojure "1.9.0"]
-   [arctype/service "1.0.0"]
-   [arctype/curator "0.0.8"]])
+   [arctype/service "1.0.4"]
+   [arctype/curator "0.1.0-SNAPSHOT"]])
